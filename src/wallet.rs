975d45cd73ea374aa46b370380237f12ed67ade9
fcihpy <13911216523@139.com>
2024-01-09 17:27:08 +0800

    简化生成BTC地址方法

--- src/wallet.rs
+++ src/wallet.rs
@@ -5,7 +5,7 @@ use hdpath::{AccountHDPath, Purpose, StandardHDPath};
 use secp256k1::Secp256k1;
 use std::convert::TryInto;
 
-pub fn get_private_key(seed: [u8; 64], purpose: u32) -> ExtendedPrivKey {
+pub fn get_private_key(seed: [u8; 64], purpose: u32,coin_type: u32) -> ExtendedPrivKey {
     let secp = Secp256k1::new();
 
     let master = ExtendedPrivKey::new_master(Network::Bitcoin, &seed).unwrap();
@@ -13,7 +13,7 @@ pub fn get_private_key(seed: [u8; 64], purpose: u32) -> ExtendedPrivKey {
 
     let path = vec![
         ChildNumber::Hardened {index:purpose},
-        ChildNumber::Hardened {index:0},
+        ChildNumber::Hardened {index:coin_type},
         ChildNumber::Hardened {index:0},
         ChildNumber::Normal {index:0},
         ChildNumber::Normal {index:0},
@@ -26,102 +26,126 @@ pub fn get_private_key(seed: [u8; 64], purpose: u32) -> ExtendedPrivKey {
     return private_key;
 }
 
-#[test]
-fn test_get_private_key() {
-    let test_mnemonic_phrase: &str =
-        "pulp gun crisp mechanic hub ahead blouse hurry life boss option evolve";
-    let test_seed = self::get_mnemonic(test_mnemonic_phrase)
-        .unwrap()
-        .to_seed("");
-    println!("bip39种子(seed): {:?}", hex::encode(test_seed));
-
-    let hd_path = AccountHDPath::new(Purpose::Pubkey, 0, 0)
-        .address_at(0, 0)
-        .unwrap();
-    let seed_byte = hex::decode(hex::encode(test_seed)).unwrap();
-    // 获取扩展私钥
-    let private_key = get_private_key(test_seed,44);
-    println!("子私钥 {}", private_key.private_key.to_string());
-
-    assert_eq!(
-        private_key.private_key.to_string(),
-        "L3sQh1LbgjxxsGW9hgSskg87MaMJWGcp4Pf8acAjbbeFSNBrPVC4"
-    )
-}
 
 pub fn get_public_key(private_key: ExtendedPrivKey) -> ExtendedPubKey {
     let secp = Secp256k1::new();
     ExtendedPubKey::from_private(&secp, &private_key)
 }
 
-#[test]
-fn test_get_public_key() {
-    let test_mnemonic_phrase: &str =
-        "pulp gun crisp mechanic hub ahead blouse hurry life boss option evolve";
-    let test_seed = get_mnemonic(test_mnemonic_phrase)
-        .unwrap()
-        .to_seed("");
 
-    let hd_path = AccountHDPath::new(Purpose::Pubkey, 0, 0)
-        .address_at(0, 0)
-        .unwrap();
-    let private_key = get_private_key(test_seed, 44);
+pub fn btc_addr_p2pkh(mnemonic: &str) -> String {
+    let mn = Mnemonic::parse_normalized(mnemonic);
+    let seed = mn.unwrap().to_seed("");
+    let private_key = get_private_key(seed, 44,0);
+    let pubkey = get_public_key(private_key);
+    return  Address::p2pkh(&pubkey.public_key, Network::Bitcoin).to_string();
 
-    let public_key = get_public_key(private_key);
-    assert_eq!(
-        "032352a1c4465934cdff949e4f0bb9a050676a9f6162ecca238612b08519bdcded",
-        public_key.public_key.to_string()
-    );
 }
 
-// pub fn btc_addr_p2pkh(pubkey: ExtendedPubKey) -> &'static str {
-//     let p2pkh: String = Address::p2pkh(&pubkey.public_key, Network::Bitcoin).to_string();
-//
-// }
+pub fn btc_addr_p2shwpkh(mnemonic: &str) -> String {
+    let mn = Mnemonic::parse_normalized(mnemonic);
+    let seed = mn.unwrap().to_seed("");
+    let private_key = get_private_key(seed, 49,0);
+    let pubkey = get_public_key(private_key);
+    return  Address::p2shwpkh(&pubkey.public_key, Network::Bitcoin).unwrap().to_string();
 
-//
-// pub fn pubkey_address(pubkey: ExtendedPubKey) -> [(&'static str, String); 3] {
-//     let p2pkh: String = Address::p2pkh(&pubkey.public_key, Network::Bitcoin).to_string();
-//
-//     let p2wpkh: String = Address::p2wpkh(&pubkey.public_key, Network::Bitcoin)
-//         .unwrap()
-//         .to_string();
-//
-//     let p2shwpkh: String = Address::p2shwpkh(&pubkey.public_key, Network::Bitcoin)
-//         .unwrap()
-//         .to_string();
-//
-//     [("p2pkh", p2pkh), ("p2wpkh", p2wpkh), ("p2shwpkh", p2shwpkh)]
-// }
-//
-// //
-// #[test]
-// fn test_pubkey_address() {
-//     let test_mnemonic_phrase: &str =
-//         "pulp gun crisp mechanic hub ahead blouse hurry life boss option evolve";
-//     let test_seed = self::get_mnemonic(test_mnemonic_phrase)
-//         .unwrap()
-//         .to_seed("");
-//
-//     let hd_path = AccountHDPath::new(Purpose::Pubkey, 0, 0)
-//         .address_at(0, 0)
-//         .unwrap();
-//     let private_key = get_private_key(test_seed, &hd_path);
-//
-//     let public_key = get_public_key(private_key);
-//
-//     let expected_results = [
-//         ["p2pkh", "1NDScYSjEqrYAMGRr7DwLhwFRACqo1BCU1"],
-//         ["p2wpkh", "bc1qdk8g0wn5lnvuf6da2rxfk5922285qje3tz7dca"],
-//         ["p2shwpkh", "33TPM4YMjigYdFE3J1zeVk7Y3pyBgXnNT9"],
-//     ];
-//     let addresses = pubkey_address(public_key);
-//     for (i, address) in IntoIterator::into_iter(addresses).enumerate() {
-//         assert_eq!(expected_results[i][0], address.0);
-//         assert_eq!(expected_results[i][1], address.1);
-//     }
-// }
-//
+}
+
+pub fn btc_addr_p2wpkh(mnemonic: &str) -> String {
+    let mn = Mnemonic::parse_normalized(mnemonic);
+    let seed = mn.unwrap().to_seed("");
+    let private_key = get_private_key(seed, 84,0);
+    let pubkey = get_public_key(private_key);
+    return  Address::p2wpkh(&pubkey.public_key, Network::Bitcoin).unwrap().to_string();
+}
+
+pub fn eth_addr(mnemonic: &str) -> String {
+    let mn = Mnemonic::parse_normalized(mnemonic);
+    let seed = mn.unwrap().to_seed("");
+    let private_key = get_private_key(seed, 44,60);
+    let pubkey = get_public_key(private_key);
+    println!("ssss {}", private_key.private_key.to_string());
+    return  Address::p2wpkh(&pubkey.public_key, Network::Bitcoin).unwrap().to_string();
+}
+
+#[cfg(test)]
+pub mod tests {
+    use hdpath::{AccountHDPath, Purpose};
+    use crate::wallet::{btc_addr_p2pkh, btc_addr_p2shwpkh, btc_addr_p2wpkh, eth_addr, get_mnemonic, get_private_key, get_public_key};
+
+    #[test]
+    fn test_p2p2kh_addr() {
+        let mn = "pulp gun crisp mechanic hub ahead blouse hurry life boss option evolve";
+        let addr = btc_addr_p2pkh(mn);
+        assert_eq!(addr, "1NDScYSjEqrYAMGRr7DwLhwFRACqo1BCU1")
+    }
+
+    #[test]
+    fn test_p2shwpkh_addr() {
+        let mn = "pulp gun crisp mechanic hub ahead blouse hurry life boss option evolve";
+        let addr = btc_addr_p2shwpkh(mn);
+        assert_eq!(addr, "33TPM4YMjigYdFE3J1zeVk7Y3pyBgXnNT9")
+    }
+
+
+    #[test]
+    fn test_p2wpkh_addr() {
+        let mn = "pulp gun crisp mechanic hub ahead blouse hurry life boss option evolve";
+        let addr = btc_addr_p2wpkh(mn);
+        assert_eq!(addr, "bc1qdk8g0wn5lnvuf6da2rxfk5922285qje3tz7dca")
+    }
+
+    #[test]
+    fn test_eth_addr() {
+        let mn = "pulp gun crisp mechanic hub ahead blouse hurry life boss option evolve";
+        let addr = eth_addr(mn);
+        assert_eq!(addr, "bc1qdk8g0wn5lnvuf6da2rxfk5922285qje3tz7dca")
+    }
+
+    #[test]
+    fn test_get_private_key() {
+        let test_mnemonic_phrase: &str =
+            "pulp gun crisp mechanic hub ahead blouse hurry life boss option evolve";
+        let test_seed = self::get_mnemonic(test_mnemonic_phrase)
+            .unwrap()
+            .to_seed("");
+        println!("bip39种子(seed): {:?}", hex::encode(test_seed));
+
+        let hd_path = AccountHDPath::new(Purpose::Pubkey, 0, 0)
+            .address_at(0, 0)
+            .unwrap();
+        let seed_byte = hex::decode(hex::encode(test_seed)).unwrap();
+        // 获取扩展私钥
+        let private_key = get_private_key(test_seed,44,0);
+        println!("子私钥 {}", private_key.private_key.to_string());
+
+        assert_eq!(
+            private_key.private_key.to_string(),
+            "L3sQh1LbgjxxsGW9hgSskg87MaMJWGcp4Pf8acAjbbeFSNBrPVC4"
+        )
+    }
+
+    #[test]
+    fn test_get_public_key() {
+        let test_mnemonic_phrase: &str =
+            "pulp gun crisp mechanic hub ahead blouse hurry life boss option evolve";
+        let test_seed = get_mnemonic(test_mnemonic_phrase)
+            .unwrap()
+            .to_seed("");
+
+        let hd_path = AccountHDPath::new(Purpose::Pubkey, 0, 0)
+            .address_at(0, 0)
+            .unwrap();
+        let private_key = get_private_key(test_seed, 44,0);
+
+        let public_key = get_public_key(private_key);
+        assert_eq!(
+            "032352a1c4465934cdff949e4f0bb9a050676a9f6162ecca238612b08519bdcded",
+            public_key.public_key.to_string()
+        );
+    }
+}
+
 fn get_mnemonic(mnemonic: &str) -> Result<Mnemonic, Error> {
     return Mnemonic::parse_normalized(&mnemonic);
 }
